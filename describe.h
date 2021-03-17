1840ef3ec768d9c9f9a55e5b650cd3638143bbef
Erik Kline <ekline@gmail.com>
2021-03-16 17:17:44 -0700

    describe: prettyprint raw message bytes

--- describe.h
+++ describe.h
@@ -14,6 +14,8 @@ LICENSE_END */
 #define MCAST_DESCRIBE_H
 
 
+#include <cctype>
+#include <cstdio>
 #include <sstream>
 #include <string>
 
@@ -30,6 +32,45 @@ std::string describe(const socket::Msg msg, ssize_t rcvd) {
     std::stringstream str{};
 
     str << "received " << rcvd << " bytes from " << socket::to_string(msg.ss);
+
+    const int bytes_per_line{16};
+    const std::string indent{"    "};
+    char buf[3]{};
+    for (int i = 0; i < rcvd; i++) {
+        if (i == 0) str << "\ndata:";
+        str << "\n";
+
+        str << indent;
+        for (int j = 0; j < bytes_per_line; j++) {
+            if (j % 4 == 0) str << " ";
+            if (i + j < rcvd) {
+                std::snprintf(buf, 3, "%02x", msg.pckt[i + j]);
+            } else {
+                std::snprintf(buf, 3, "  ");
+            }
+            buf[2] = '\0';
+            str << buf;
+        }
+
+        str << indent;
+        for (int j = 0; j < bytes_per_line; j++) {
+            if (j % 4 == 0) str << " ";
+            if (i + j < rcvd) {
+                if (std::isgraph(msg.pckt[i + j]) != 0) {
+                    std::snprintf(buf, 2, "%c", msg.pckt[i + j]);
+                } else {
+                    std::snprintf(buf, 2, ".");
+                }
+            } else {
+                std::snprintf(buf, 2, " ");
+            }
+            buf[1] = '\0';
+            str << buf;
+        }
+
+        i += bytes_per_line;
+    }
+    str << "\n";
 
     return str.str();
 }
